18671a88c067fbe835785123694c954b49bb7eeb
Kinaz Rezek <67725394+krezek@users.noreply.github.com>
2021-05-11 15:08:00 +0200

    Adding TreeWalker function

--- SimplifyCalculator/src/editor.c
+++ SimplifyCalculator/src/editor.c
@@ -4,13 +4,17 @@
 #include <parser.h>
 
 static void OnInitialize(Editor* ed);
+static void OnStopEditing(Editor* ed);
 
-EditorNode* EditorNode_init(Item* itm, NodeType nt, String* str, int index, EditorNode* n, EditorNode* p)
+static void Update_ItemsOrder(Editor* ed);
+static void TreeWalker(Editor* ed, Item** pItem);
+
+EditorNode* EditorNode_init(Item** pIm, NodeType nt, String* str, int index, EditorNode* n, EditorNode* p)
 {
 	EditorNode* en = (EditorNode*)malloc(sizeof(EditorNode));
 	assert(en);
 
-	en->_item = itm;
+	en->_pItem = pIm;
 	en->_nodeType = nt;
 	en->_str = str;
 	en->_index = index;
@@ -36,16 +40,16 @@ EditorLinkedList* LinkedList_init()
 	return ll;
 }
 
-void LinkedList_pushpack(EditorLinkedList* ll, Item* itm, NodeType nt, String* str, int index)
+void LinkedList_pushpack(EditorLinkedList* ll, Item** pIm, NodeType nt, String* str, int index)
 {
 	if (ll->_rear == NULL)
 	{
 		assert(ll->_front == NULL);
-		ll->_front = ll->_rear = EditorNode_init(itm, nt, str, index, NULL, NULL);
+		ll->_front = ll->_rear = EditorNode_init(pIm, nt, str, index, NULL, NULL);
 	}
 	else
 	{
-		EditorNode* i = EditorNode_init(itm, nt, str, index, NULL, ll->_rear);
+		EditorNode* i = EditorNode_init(pIm, nt, str, index, NULL, ll->_rear);
 		ll->_rear->_next = i;
 		ll->_rear = i;
 	}
@@ -84,12 +88,19 @@ Editor* Editor_init(Item** pItems)
 	ed->_current_node = NULL;
 
 	ed->_OnEditorInitializeFunc = OnInitialize;
+	ed->_OnStopEditingFunc = OnStopEditing;
 
 	return ed;
 }
 
 void Editor_free(Editor* ed)
 {
+	if (ed->_itemsOrder)
+	{
+		LinkedList_free(ed->_itemsOrder);
+		ed->_itemsOrder = NULL;
+	}
+
 	free(ed);
 }
 
@@ -100,4 +111,90 @@ static void OnInitialize(Editor* ed)
 	{
 		wprintf(L"Editor:OnInitialize:parse Error\n");
 	}
+
+	Update_ItemsOrder(ed);
 }
+
+static void OnStopEditing(Editor* ed)
+{
+
+}
+
+static void Update_ItemsOrder(Editor* ed)
+{
+	if (ed->_itemsOrder)
+	{
+		LinkedList_free(ed->_itemsOrder);
+	}
+
+	ed->_itemsOrder = LinkedList_init();
+
+	TreeWalker(ed, ed->_pItems);
+}
+
+void TreeWalker(Editor* ed, Item** pItem)
+{
+	if ((*pItem)->_objectType == OBJ_Add ||
+		(*pItem)->_objectType == OBJ_Sub ||
+		(*pItem)->_objectType == OBJ_Mult ||
+		(*pItem)->_objectType == OBJ_Frac ||
+		(*pItem)->_objectType == OBJ_Pow ||
+		(*pItem)->_objectType == OBJ_Subscript ||
+		(*pItem)->_objectType == OBJ_List ||
+		(*pItem)->_objectType == OBJ_Equ)
+	{
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_Begin, NULL, 0);
+		TreeWalker(ed, &(*pItem)->_left);
+		TreeWalker(ed, &(*pItem)->_right);
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_End, NULL, 0);
+	}
+	else if ((*pItem)->_objectType == OBJ_Parentheses ||
+		(*pItem)->_objectType == OBJ_Sign ||
+		(*pItem)->_objectType == OBJ_Factorial ||
+		(*pItem)->_objectType == OBJ_Sqrt ||
+		(*pItem)->_objectType == OBJ_CommFunc)
+	{
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_Begin, NULL, 0);
+		TreeWalker(ed, &(*pItem)->_left);
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_End, NULL, 0);
+	}
+	else if ((*pItem)->_objectType == OBJ_Sigma)
+	{
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_Begin, NULL, 0);
+		if (((ItemSigma*)((*pItem)))->_bottom)
+			TreeWalker(ed, &((ItemSigma*)((*pItem)))->_bottom);
+		if (((ItemSigma*)((*pItem)))->_top)
+			TreeWalker(ed, &((ItemSigma*)((*pItem)))->_top);
+		TreeWalker(ed, &(*pItem)->_left);
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_End, NULL, 0);
+	}
+	else if ((*pItem)->_objectType == OBJ_Integrate)
+	{
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_Begin, NULL, 0);
+		if (((ItemIntegrate*)((*pItem)))->_bottom)
+			TreeWalker(ed, &((ItemIntegrate*)((*pItem)))->_bottom);
+		if (((ItemIntegrate*)((*pItem)))->_top)
+			TreeWalker(ed, &((ItemIntegrate*)((*pItem)))->_top);
+		TreeWalker(ed, &(*pItem)->_left);
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_End, NULL, 0);
+	}
+	else if ((*pItem)->_objectType == OBJ_Number)
+	{
+		ItemNumber* i = (ItemNumber*)(*pItem);
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_Number, i->_str, 0);
+	}
+	else if ((*pItem)->_objectType == OBJ_Literal)
+	{
+		ItemLiteral* i = (ItemLiteral*)(*pItem);
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_Literal, i->_str, 0);
+	}
+	else if ((*pItem)->_objectType == OBJ_Base)
+	{
+		LinkedList_pushpack(ed->_itemsOrder, pItem, NT_Null, NULL, 0);
+	}
+	else
+	{
+		wprintf(L"Editor_TreeWalker: error\n");
+	}
+}
+
--- SimplifyCalculator/src/editor.h
+++ SimplifyCalculator/src/editor.h
@@ -8,10 +8,11 @@ typedef enum { NT_Null, NT_Begin, NT_End, NT_Number, NT_Literal } NodeType;
 typedef struct _Editor Editor;
 
 typedef void (*OnEditorInitializeFunc) (Editor* ed);
+typedef void (*OnStopEditingFunc) (Editor* ed);
 
 typedef struct _EditorNode
 {
-	Item* _item;
+	Item** _pItem;
 
 	NodeType _nodeType;
 	String* _str;
@@ -35,6 +36,7 @@ typedef struct _Editor
 	EditorNode* _current_node;
 
 	OnEditorInitializeFunc _OnEditorInitializeFunc;
+	OnStopEditingFunc _OnStopEditingFunc;
 } Editor;
 
 Editor* Editor_init(Item** pitems);
